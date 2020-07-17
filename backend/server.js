6464848fa942580aec9a087f4066f90104f1c5fa
Cobraaz <anujbansal077@gmail.com>
2020-07-17 11:41:40 +0530

    commiting amazon-clone

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import data from "./data";
+import keys from "../config/keys";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+
+const mongodbUrl = keys.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+// app.get("/api/products/:id", (req, res) => {
+//   const productId = req.params.id;
+//   const product = data.products.find(x => x._id === productId);
+//   if (product)
+//     res.send(product);
+//   else
+//     res.status(404).send({ msg: "Product Not Found." })
+// });
+
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
+
+app.listen(5001, () => {
+  console.log("Server started at http://localhost:5001");
+});
